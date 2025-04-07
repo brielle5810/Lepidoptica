20787e5a458a12c888a4d767e74e4a414bfa41ea
Alexia Rangel Krashenitsa <73617301+alexiarangelk@users.noreply.github.com>
2025-04-07 14:11:45 -0400

    All work done so far for receiving nanodata

--- box-nanonets.py
+++ box-nanonets.py
@@ -4,10 +4,12 @@ import requests
 from PIL import Image, ImageOps
 
 model = NANONETSOCR()
-# MODEL_ID = '5c1490c0-0edc-412e-949c-0e68060b76db'
-# API_KEY = '10fe9db1-f1f6-11ef-b958-eaf9f4bd4f51'
-MODEL_ID = '52e3f05e-50dd-430a-9b8d-42f28e0942b2'
-API_KEY = '6ef92718-f203-11ef-90b1-a6fe692a5476'
+#KATJA:
+# MODEL_ID = '52e3f05e-50dd-430a-9b8d-42f28e0942b2'
+# API_KEY = '6ef92718-f203-11ef-90b1-a6fe692a5476'
+#BRIELLE:
+MODEL_ID = '5c1490c0-0edc-412e-949c-0e68060b76db'
+API_KEY = '10fe9db1-f1f6-11ef-b958-eaf9f4bd4f51'
 model.set_token(API_KEY)
 # MODEL_ID = '5c1490c0-0edc-412e-949c-0e68060b76db'
 # API_KEY = '10fe9db1-f1f6-11ef-b958-eaf9f4bd4f51'
@@ -22,13 +24,13 @@ response = requests.post(url, auth=(API_KEY, ''), headers={"Content-Type": "appl
 response_data = response.json()
 #print (response_data)
 
-PREPROCESSED_DIR = "preprocessed_K"
+PREPROCESSED_DIR = "preprocessed_B"
 OUTPUT_DIR = "NANONETS_DATA4"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 for image in response_data["moderated_images"]:
     image_filename = os.path.basename(image["original_file_name"])
-    #image_basename, _ = os.path.splitext(image_filename)
+    # image_basename, _ = os.path.splitext(image_filename)
     original_image_filename = image_filename
 
     image_filename = image_filename.replace(" ", "_")
@@ -37,7 +39,6 @@ for image in response_data["moderated_images"]:
     print(f"Processing image {image_filename}...")
 
     # corresponding image from preprocessed directory
-    # image_path = os.path.join(PREPROCESSED_DIR, image_filename)
     image_path = os.path.join(PREPROCESSED_DIR, original_image_filename)
 
     if not os.path.exists(image_path):
@@ -49,28 +50,30 @@ for image in response_data["moderated_images"]:
 
     for box in image["moderated_boxes"]:
 
-        box_id = box["id"]
+        try:
 
-        # tiff_filename = f"{image_basename}_{box_id}.tiff"
-        # tiff_path = os.path.join(OUTPUT_DIR, tiff_filename)
-        tiff_filename = f"{image_basename}_{box_id}.tiff".replace(" ", "_")
-        tiff_path = os.path.join(OUTPUT_DIR, tiff_filename)
+            box_id = box["id"]
 
-        if not os.path.exists(tiff_path):
-            xmin, ymin, xmax, ymax = box["xmin"], box["ymin"], box["xmax"], box["ymax"]
-            print(f"Box {box_id} coordinates: ({xmin}, {ymin}) - ({xmax}, {ymax})")
-            ocr_text = box["ocr_text"]
+            png_filename = f"{image_basename}_{box_id}.png".replace(" ", "_")
+            png_path = os.path.join(OUTPUT_DIR, png_filename)
 
-            print(f"Processing box {box_id} from {image_filename}...")
-            cropped_img = img.crop((xmin, ymin, xmax, ymax))
-            cropped_img.save(tiff_path, format="TIFF")
-            # gt_filename = f"{image_basename}_{box_id}.gt.txt"
-            gt_filename = f"{image_basename}_{box_id}.gt.txt".replace(" ", "_")
-            gt_path = os.path.join(OUTPUT_DIR, gt_filename)
-            with open(gt_path, "w", encoding="utf-8") as txt_file:
-                txt_file.write(ocr_text)
-        else:
-            print(f"File already exists: {tiff_path}")
+            if not os.path.exists(png_path):
+                xmin, ymin, xmax, ymax = box["xmin"], box["ymin"], box["xmax"], box["ymax"]
+                print(f"Box {box_id} coordinates: ({xmin}, {ymin}) - ({xmax}, {ymax})")
+                ocr_text = box["ocr_text"]
 
+                print(f"Processing box {box_id} from {image_filename}...")
+                cropped_img = img.crop((xmin, ymin, xmax, ymax))
+                cropped_img.save(png_path, format="PNG")
+                # gt_filename = f"{image_basename}_{box_id}.gt.txt"
+                gt_filename = f"{image_basename}_{box_id}.gt.txt".replace(" ", "_")
+                gt_path = os.path.join(OUTPUT_DIR, gt_filename)
+                with open(gt_path, "w", encoding="utf-8") as txt_file:
+                    txt_file.write(ocr_text)
+            else:
+                print(f"File already exists: {png_path}")
+
+        except Exception as e:
+            print(f"Error processing image: {e}")
 
 print(f"Processed images saved in '{OUTPUT_DIR}'.")
